952d773fcf025cfe491ca936feb537cef1d7a25d
Ruan Pablo Pionte <ruanpablopionte@gmail.com>
2024-04-17 20:18:06 -0300

    feat: create the authenticate function

--- app/lib/actions/user.js
+++ app/lib/actions/user.js
@@ -3,6 +3,7 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { connectToDB } from "../utils";
 import { User } from "../models/user";
+import { signIn } from "@/auth";
 
 export const addUser = async (formData) => {
   const { username, email, password } = Object.fromEntries(formData);
@@ -25,4 +26,18 @@ export const addUser = async (formData) => {
 
   revalidatePath("/Finance");
   redirect("/Finance");
+};
+
+export const authenticate = async (prevState, formData) => {
+  // console.log(formData);
+  const { username, password } = Object.fromEntries(formData);
+
+  try {
+    await signIn("credentials", { username, password });
+  } catch (err) {
+    if (err.message.includes("CredentialsSignin")) {
+      return "Wrong Credentials";
+    }
+    throw err;
+  }
 };
