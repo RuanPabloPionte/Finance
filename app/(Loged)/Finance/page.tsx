184b58c1b89f09677e4379c2b822b438e5a8a0c4
Ruan Pablo Pionte <ruanpablopionte@gmail.com>
2024-04-18 19:24:29 -0300

    feat: add table from shadcn

---
+++ app/(Loged)/Finance/page.tsx
@@ -0,0 +1,40 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/app/components/ui/table";
+
+function Finance() {
+  return (
+    <section className=" m-4 border-2 border-red-500">
+      {/* transactions */}
+      <div className="border-2 border-blue-500">
+        <Table>
+          <TableCaption>Uma lista de transações.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Invoice</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Method</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">INV001</TableCell>
+              <TableCell>Paid</TableCell>
+              <TableCell>Credit Card</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+    </section>
+  );
+}
+
+export default Finance;
