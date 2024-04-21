451dcbdbc0f1bdd07863b6f43750d7dde9a05a62
Ruan Pablo Pionte <ruanpablopionte@gmail.com>
2024-04-21 12:27:24 -0300

    fix: chage the attributes from transactions

--- app/lib/models/transactions.js
+++ app/lib/models/transactions.js
@@ -1,9 +1,31 @@
-const mongoose = require('mongoose')
+import mongoose from "mongoose";
 
-const TransactionSchema = mongoose.Schema({
-    name:{type: String, require: true},
-    value:{type: Number, require: true},
-    createdAt:{type: Date, default: Date.now()},
-})
+// Define schema for transaction types
+const transactionTypeSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+});
 
-export const Transaction = mongoose.models.Transaction || mongoose.model("Transaction",TransactionSchema)
+// Create model for transaction types
+const TransactionType =
+  mongoose.models.TransactionType ||
+  mongoose.model("TransactionType", transactionTypeSchema);
+
+// Define schema for transactions
+const transactionSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  type: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "TransactionType",
+    required: true,
+  },
+  source: { type: String, required: true },
+  value: { type: Number, required: true },
+  createdAt: { type: Date, default: Date.now() },
+});
+
+// Create model for transactions
+const Transaction =
+  mongoose.models.Transaction ||
+  mongoose.model("Transaction", transactionSchema);
+
+export { Transaction, TransactionType };
