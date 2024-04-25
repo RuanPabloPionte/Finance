628179d38aedda09c8e787d09aa4941a4a715fc7
Ruan Pablo Pionte <ruanpablopionte@gmail.com>
2024-04-25 19:47:15 -0300

    feat: display the transaction data on the table

--- app/components/TransactionsTable.tsx
+++ app/components/TransactionsTable.tsx
@@ -19,56 +19,63 @@ import { Button } from "@/app/components/ui/button";
 
 import { HiDotsVertical } from "react-icons/hi";
 import { FaTrashCan, FaPen } from "react-icons/fa6";
+import { getUser } from "../lib/actions/user";
+import { auth } from "@/auth";
+import { Session } from "next-auth";
+import { getTransactionTypes } from "../lib/actions/Transactions";
 
-const data = [
-  {
-    category: "GASOLINA",
-    source: "PIC PAY",
-    type: "deposit/whidraw",
-    amount: "-$60.00",
-  },
-  { category: "pneu", source: "PIC PAY", amount: "-$250.00" },
-];
+async function TransactionsTable() {
+  const session: Session | null = await auth();
+  const user = await getUser(session?.user?.id);
 
-function TransactionsTable() {
   return (
     <Table>
       <TableHeader>
         <TableRow>
           <TableHead className="w-[100px]">NOME</TableHead>
-          <TableHead>INSTITUIÇÃO</TableHead>
+          <TableHead>DESTINO</TableHead>
+          <TableHead>TIPO</TableHead>
           <TableHead className="text-right">VALOR</TableHead>
           <TableHead className="text-right"></TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        {data.map((item, index) => (
-          <TableRow key={index}>
-            <TableCell className="font-medium">
-              {item.category.toLocaleUpperCase()}
-            </TableCell>
-            <TableCell>{item.source.toLocaleUpperCase()}</TableCell>
-            <TableCell className="text-right">{item.amount}</TableCell>
-            <TableCell className="text-right">
-              <Popover>
-                <PopoverTrigger>
-                  <HiDotsVertical />
-                </PopoverTrigger>
-                <PopoverContent className="my-2 mx-4 w-[30vw]">
-                  <h1 className="text-center">Ações disponiveis</h1>
-                  <div className="mt-2 grid gap-4">
-                    <Button>
-                      DELETAR <FaTrashCan className="mx-2" />
-                    </Button>
-                    <Button>
-                      EDITAR <FaPen className="mx-2" />
-                    </Button>
-                  </div>
-                </PopoverContent>
-              </Popover>
-            </TableCell>
-          </TableRow>
-        ))}
+        {user.transactions.map(async (item: any) => {
+          const transactionTypes = await getTransactionTypes(item.type);
+          return (
+            <TableRow
+              key={item.id}
+              className={`${
+                item.value < 0 ? "text-red-500" : "text-emerald-500"
+              }`}
+            >
+              <TableCell className="font-medium">
+                {item.name.toLocaleUpperCase()}
+              </TableCell>
+              <TableCell>{item.source.toLocaleUpperCase()}</TableCell>
+              <TableCell>{transactionTypes.name}</TableCell>
+              <TableCell className={`text-right `}>{item.value}</TableCell>
+              <TableCell className="text-right">
+                <Popover>
+                  <PopoverTrigger>
+                    <HiDotsVertical />
+                  </PopoverTrigger>
+                  <PopoverContent className="my-2 mx-4 w-[30vw]">
+                    <h1 className="text-center">Ações disponiveis</h1>
+                    <div className="mt-2 grid gap-4">
+                      <Button>
+                        DELETAR <FaTrashCan className="mx-2" />
+                      </Button>
+                      <Button>
+                        EDITAR <FaPen className="mx-2" />
+                      </Button>
+                    </div>
+                  </PopoverContent>
+                </Popover>
+              </TableCell>
+            </TableRow>
+          );
+        })}
       </TableBody>
     </Table>
   );
